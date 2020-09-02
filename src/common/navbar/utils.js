945a8b1c3f8e4773226b6f3a691a17fde1f2d3b3
AnanthFrancis <ananth.francis@axa.com.sg>
2020-09-02 16:50:57 +0800

    added grid details page

--- src/common/navbar/utils.js
+++ src/common/navbar/utils.js
@@ -1,68 +1,73 @@
 export const metaDataNavbar = [
-    {
-        id: "1",
-        navText: "Dashboard",
-        iconName: "faHome",
-        hasSubNav: false,
-        route: "dashboard",
-    },
-    {
-        id: "2",
-        navText: "Grid",
-        iconName: "faThLarge",
-        hasSubNav: true,
-        route: "dashboard",
-        subNavs: [
-            {
-                id: "gridSub" + 1,
-                navText: "Grid Layer - DPR",
-                route: "griddpr",
-            },
-            {
-                id: "gridSub" + 2,
-                navText: "Create Grid",
-                route: "creategrid",
-            },
-            {
-                id: "gridSub" + 3,
-                navText: "View Grid",
-                route: "viewgrid",
-            }
-        ]
-    },
-    {
-        id: "3",
-        navText: "Client Billing",
-        iconName: "faFileInvoiceDollar",
-        hasSubNav: false,
-        route: "clientbilling",
-    },
-    {
-        id: "4",
-        navText: "Sub-Contractor",
-        iconName: "faUser",
-        hasSubNav: false,
-        route: "subcontractor",
-    },
-    {
-        id: "5",
-        navText: "Reports",
-        iconName: "faChartPie",
-        hasSubNav: false,
-        route: "dashboard",
-    },
-    {
-        id: "6",
-        navText: "Help",
-        iconName: "faQuestionCircle",
-        hasSubNav: false,
-        route: "dashboard",
-    },
-    {
-        id: "7",
-        navText: "Settings",
-        iconName: "faCog",
-        hasSubNav: false,
-        route: "login",
-    }
-]
+  {
+    id: '1',
+    navText: 'Dashboard',
+    iconName: 'faHome',
+    hasSubNav: false,
+    route: 'dashboard',
+  },
+  {
+    id: '2',
+    navText: 'Grid',
+    iconName: 'faThLarge',
+    hasSubNav: true,
+    route: 'dashboard',
+    subNavs: [
+      {
+        id: 'gridSub' + 1,
+        navText: 'Grid Layer - DPR',
+        route: 'griddpr',
+      },
+      {
+        id: 'gridSub' + 2,
+        navText: 'Create Grid',
+        route: 'creategrid',
+      },
+      {
+        id: 'gridSub' + 3,
+        navText: 'View Grid',
+        route: 'viewgrid',
+      },
+      {
+        id: 'gridSub' + 4,
+        navText: 'Grid Details',
+        route: 'griddetails',
+      },
+    ],
+  },
+  {
+    id: '3',
+    navText: 'Client Billing',
+    iconName: 'faFileInvoiceDollar',
+    hasSubNav: false,
+    route: 'clientbilling',
+  },
+  {
+    id: '4',
+    navText: 'Sub-Contractor',
+    iconName: 'faUser',
+    hasSubNav: false,
+    route: 'subcontractor',
+  },
+  {
+    id: '5',
+    navText: 'Reports',
+    iconName: 'faChartPie',
+    hasSubNav: false,
+    route: 'dashboard',
+  },
+  {
+    id: '6',
+    navText: 'Help',
+    iconName: 'faQuestionCircle',
+    hasSubNav: false,
+    route: 'dashboard',
+  },
+  {
+    id: '7',
+    navText: 'Settings',
+    iconName: 'faCog',
+    hasSubNav: false,
+    route: 'login',
+  },
+];
--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from "./rootReducer";
-let middlewares = []
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+let middlewares = [];
 if (process.env.NODE_ENV === `local` || process.env.NODE_ENV === `dev`) {
   const { logger } = require(`redux-logger`);
   middlewares.push(logger);
 }
 middlewares.push(thunk);
 export default function storeConfig(initialState = {}) {
-  return createStore(rootReducer, initialState, applyMiddleware(...middlewares));
+  return createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middlewares)
+  );
 }
