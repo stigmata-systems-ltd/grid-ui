5d666f23a75b75f21951fe251cad8f0a81f569b9
AnanthFrancis <ananth.francis@axa.com.sg>
2020-09-05 09:41:01 +0800

    removed unwanted codes

--- src/actions/subContractorActions.js
+++ src/actions/subContractorActions.js
@@ -1,30 +1,6 @@
-import {
-  ADD_SUBCONTRACTOR,
-  SUBCONTRACTOR_NAME,
-  SUBCONTRACTOR_CODE,
-  SUBCONTRACTOR_CONTACT_PERSON,
-  SUBCONTRACTOR_CONTACT_ADDRESS,
-  SUBCONTRACTOR_PHONE,
-  SUBCONTRACTOR_EMAIL,
-} from './types';
+import { ADD_SUBCONTRACTOR } from './types';
 import store from '../store';
 import axios from 'axios';
-// export const addSubContractor = () => dispatch => {
-//   fetch('http://127.0.0.1:4010/api/SubCont/AddSubcontractor', {
-//     method: 'POST',
-//     headers: {
-//       'content-type': 'application/json',
-//     },
-//     body: JSON.stringify(postData),
-//   })
-//     .then(res => res.json())
-//     .then(post =>
-//       dispatch({
-//         type: ADD_SUBCONTRACTOR,
-//         payload: post,
-//       })
-//     );
-// };
 
 export const addSubContractor = () => {
   const {
