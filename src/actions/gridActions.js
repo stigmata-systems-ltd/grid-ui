42377d383c21357bfd74f1124197319af1da7b55
ananth2307 <beyonceptiontestmail@gmail.com>
2020-09-05 20:57:42 +0530

    Added Promise Middleware

--- src/actions/gridActions.js
+++ src/actions/gridActions.js
@@ -12,17 +12,8 @@ import axios from 'axios';
 // };
 
 export const gridNoList = () => dispatch => {
-  fetch('http://127.0.0.1:4010/api/Grid/GridNoList', {
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-  })
-    .then(res => res.json())
-    .then(posts =>
-      dispatch({
-        type: GRID_NO_LIST,
-        payload: posts,
-      })
-    );
-};
+  return {
+    type: GRID_NO_LIST,
+    payload: axios.get('http://127.0.0.1:4010/api/Grid/GridNoList')
+  }
+}
--- src/store.js
+++ src/store.js
@@ -1,17 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
+import promise from "redux-promise-middleware";
 let middlewares = [];
 if (process.env.NODE_ENV === `local` || process.env.NODE_ENV === `dev`) {
   const { logger } = require(`redux-logger`);
   middlewares.push(logger);
 }
 middlewares.push(thunk);
+middlewares.push(promise);
 export default createStore(
   rootReducer,
   {},
   compose(
-    applyMiddleware(...middlewares),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middlewares)
   )
 );
