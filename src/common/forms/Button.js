bdb5f16f7ed744ced72181a897bc4600735c02ac
AnanthFrancis <ananth.francis@axa.com.sg>
2020-09-30 15:25:26 +0800

    trying to disable and enable buttons

--- src/common/forms/Button.js
+++ src/common/forms/Button.js
@@ -1,16 +1,23 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { NONAME } from 'dns';
 
 class Button extends Component {
-
-    render() {
-        return(
-            <button 
-                type="button" 
-                class={`btn mb-2 ${this.props.btnType === "primary" ? "btn-primary" : this.props.btnType}`}
-                onClick={this.props.onClick}
-            >{this.props.btnText}</button>
-        )
-    }
+  render() {
+    const isDisable = this.props.disable && 'disabled';
+    console.log(`isDisabled: ${isDisable}`);
+    return (
+      <button
+        type="button"
+        class={`btn mb-2 ${
+          this.props.btnType === 'primary' ? 'btn-primary' : this.props.btnType
+        }`}
+        onClick={this.props.onClick}
+        isDisable
+      >
+        {this.props.btnText}
+      </button>
+    );
+  }
 }
 
 export default Button;
