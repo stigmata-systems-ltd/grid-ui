c67fdd63248ae756bc1eee53b5e20d86b791c238
AnanthFrancis <ananth.francis@axa.com.sg>
2020-10-26 10:21:33 +0800

    layer map polygon

--- src/pages/reports/LayerMapReport.js
+++ src/pages/reports/LayerMapReport.js
@@ -18,6 +18,18 @@ import {
   MAP_MARKER_PROGRESS,
   MAP_MARKER_START,
 } from '../../assets/images/index';
+import {
+  GRID_ACTIVE,
+  GRID_BILLED,
+  GRID_COMPLETED,
+  GRID_IN_PROGRESS,
+  GRID_YET_TO_START,
+  GRID_ACTIVE_STROKE,
+  GRID_BILLED_STROKE,
+  GRID_COMPLETED_STROKE,
+  GRID_IN_PROGRESS_STROKE,
+  GRID_YET_TO_START_STROKE,
+} from '../../utils/globalConst';
 import FormRow from '../../common/forms/FormRow';
 import SearchableDropDown from '../../common/forms/SearchableDropDown';
 import { transformLayerList } from './utils';
@@ -65,7 +77,42 @@ class GridMap extends Component {
       <FaIcon iconName="faPeopleCarry" className="txt-danger" />
     );
   };
+  getPolygonStrokeColor = gridSts => {
+    // console.log(`Grid Status: ${gridSts}`);
+    if (gridSts === 'InProgress') {
+      return GRID_IN_PROGRESS_STROKE;
+    } else if (gridSts === 'New') {
+      return GRID_YET_TO_START_STROKE;
+    } else if (gridSts === 'Completed' || gridSts === 'LayerCompleted') {
+      console.log(
+        `Inside LayerCompleted Section AND GRID_COMPLETED: ${GRID_COMPLETED}`
+      );
+      return GRID_COMPLETED_STROKE;
+    } else if (gridSts === 'Billed') {
+      return GRID_BILLED_STROKE;
+    } else {
+      return GRID_YET_TO_START_STROKE;
+    }
+  };
+  getPolygonFillColor = gridSts => {
+    // console.log(`Grid Status: ${gridSts}`);
+    if (gridSts === 'InProgress') {
+      return GRID_IN_PROGRESS;
+    } else if (gridSts === 'New') {
+      return GRID_YET_TO_START;
+    } else if (gridSts === 'Completed' || gridSts === 'LayerCompleted') {
+      console.log(
+        `Inside LayerCompleted Section AND GRID_COMPLETED: ${GRID_COMPLETED}`
+      );
+      return GRID_COMPLETED;
+    } else if (gridSts === 'Billed') {
+      return GRID_BILLED;
+    } else {
+      return GRID_YET_TO_START;
+    }
+  };
   getMapMarkerColor = gridSts => {
+    console.log(`Grid Status: ${gridSts}`);
     if (gridSts === 'InProgress') {
       return MAP_MARKER_PROGRESS;
     } else if (gridSts === 'New') {
@@ -131,7 +178,7 @@ class GridMap extends Component {
                           }}
                         />
                       ))}
-                      {gridData(this.props.gridDetails).map(grid => (
+                      {/* {gridData(this.props.gridDetails).map(grid => (
                         <InfoWindow
                           marker={this.state.activeMarker}
                           visible={this.state.showingInfoWindow}
@@ -139,7 +186,7 @@ class GridMap extends Component {
                         >
                           <div className="col-md-12">
                             <h5>{this.state.selectedPlace.name}</h5>
-                            {/* <h6>{grid.description}</h6> */}
+                            {/* <h6>{grid.description}</h6>
                             <h6>{this.state.selectedSts}</h6>
                             <Button
                               btnText={'View'}
@@ -148,14 +195,14 @@ class GridMap extends Component {
                             />
                           </div>
                         </InfoWindow>
-                      ))}
+                      ))} */}
                       {gridData(this.props.gridDetails).map(grid => (
                         <Polygon
                           paths={grid.rectCords}
-                          strokeColor="#0000FF"
+                          strokeColor={this.getPolygonStrokeColor(grid.status)}
                           strokeOpacity={0.8}
                           strokeWeight={2}
-                          fillColor="#0000FF"
+                          fillColor={this.getPolygonFillColor(grid.status)}
                           fillOpacity={0.35}
                         />
                       ))}
